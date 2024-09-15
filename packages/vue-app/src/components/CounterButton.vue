f4496e74e143dd20e419f9d39796062efcbcd3ff
chhw130 <527coco@naver.com>
2024-09-15 16:34:27 +0900

    feat : vue counter

---
+++ packages/vue-app/src/components/CounterButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="counter-button" @click="onClickIncrement">
+    Increment
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface CounterButtonProps {
+  onClickIncrement : () => void
+}
+
+defineProps<CounterButtonProps>()
+
+</script>
+
---
+++ packages/vue-app/src/components/CounterDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2 class="counter-display">Count: {{ count }}</h2>
+</template>
+
+<script setup lang="ts">
+import {  defineProps } from 'vue';
+
+interface  CounterDisplayProps {
+  count : number
+}
+
+ defineProps<CounterDisplayProps>()
+</script>
+
